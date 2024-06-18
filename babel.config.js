471660e44222a9f7e3514a5a67dc13b3798a9cc7
pphelios <pphelios1@gmail.com>
2024-06-18 22:19:55 +0100

    first commit

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,20 @@
 module.exports = {
-  presets: ['module:@react-native/babel-preset'],
+  presets: ['module:metro-react-native-babel-preset'],
+  env: {
+    production: {
+      plugins: ['transform-remove-console'],
+    },
+  },
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./'],
+        alias: {
+          '@': './src',
+        },
+      },
+    ],
+    'react-native-reanimated/plugin',
+  ],
 };
---
+++ ios/ci_scripts/ci_post_clone.sh
@@ -0,0 +1,7 @@
+# !/bin/sh
+
+brew install cocoapods
+brew install node
+npm i
+npx pod repo update
+npx pod install
